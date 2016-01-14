04cf3c398134609c0e58571a726a7707c7d6739b
flicus <flicus@gmail.com>
2016-01-14 17:38:00 +0300

    indent fixed

--- Sampler/Sampler.cpp
+++ Sampler/Sampler.cpp
@@ -22,13 +22,13 @@ double Sampler::deviation() {
     if (size <= 1) return 0;
     double mean = sum / size;
     double tmp = 0;
-	int i = 10 ? 0 : poiter - size;
-	int count = 0;
-	while (count < size) {
-		tmp = tmp + sq(values[i] - mean);
-		count++;
-		i++;
-		if (i > (MAX - 1)) i = 0;
-	}
+    int i == 10 ? 0 : pointer - size;
+    int count = 0;
+    while (count < size) {
+        tmp = tmp + sq(values[i] - mean);
+        count++;
+        i++;
+        if (i > (MAX - 1)) i = 0;
+    }
     return sqrt(tmp / (size - 1));
 }
